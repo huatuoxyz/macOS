6d8875856d6e94f2e6ebdf0d560d83e02af72a3e
Dongri Jin <dongrify@gmail.com>
2016-11-09 17:16:15 +0900

    Fix var

--- install.sh
+++ install.sh
@@ -1,9 +1,7 @@
 #!/usr/bin/env bash
 
 function parse() {
-  s='[[:space:]]*'
-  w='[a-zA-Z0-9_]*'
-  fs="$(echo @|tr @ '\034')"
+  local s='[[:space:]]*' w='[a-zA-Z0-9_]*' fs=$(echo @|tr @ '\034')
   sed -ne "s|^\($s\)\($w\)$s:$s\"\(.*\)\"$s\$|\1$fs\2$fs\3|p" \
       -e "s|^\($s\)\($w\)$s[:-]$s\(.*\)$s\$|\1$fs\2$fs\3|p" "$1" |
   awk -F"$fs" '{
